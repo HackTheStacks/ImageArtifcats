b20aca73af608089afe673cb3dfc88cf4e9d72d1
Karol <notkarol@gmail.com>
2016-11-20 02:27:59 -0500

    cleaned up a bit

--- src/process.lua
+++ src/process.lua
@@ -11,7 +11,7 @@ local transforms = require('transforms')
 local imagenet = require('imagenet')
 local meanstd = { mean = { 0.485, 0.456, 0.406 },
                   std = { 0.229, 0.224, 0.225 }, }
-local transform = transforms.Compose{ transforms.Scale(256),
+local transformT = transforms.Compose{ transforms.Scale(256),
                                       transforms.ColorNormalize(meanstd),
                                       transforms.TenCrop(224), }
 
@@ -43,43 +43,64 @@ function prepare_image(img)
   end
 end
 
+function process(model, image_path, n_classes, position, total)
+  local basename = paths.basename(image_path, '.jpg')
+  print(string.format("%-32s %7.3f", basename, 100 * position / total))
+  
+  -- Load image and model
+  local img = load_image(image_path)
+  local prepared_img = prepare_image(img)
+  -- image.save("../" .. basename .. "_crop.jpg", prepared_img[1])
+
+  -- Process class
+  local output = model:forward(prepared_img):squeeze()
+  local classes =  {}
+  local counts =  {}
+  for cut = 1, 10 do 
+    local probs, indexes = output[cut]:topk(n_classes, true, true)
+    for n = 1, n_classes do
+      local class = imagenet[indexes[n]]
+      classes[class] = (classes[class] or 0) + probs[n]
+      counts[class] = (counts[class] or 0) + 1
+    end
+  end
+  
+  -- Print average classes
+  for class, prob in pairs(classes) do
+    classes[class] = prob / counts[class]
+  end
+
+  local features = model.modules[10].output:float()
+  torch.save("../features/" .. basename .. ".t7", {classes, features})
+end
+
 function main()
   -- Process arguments
-  local model_path = arg[1]
-  for i, image_path in ipairs(arg) do
-    if i > 1 then
-      print(string.format("%-64s %7.3f", image_path, 100 * (i - 1) / (#arg - 1)))
-      -- Other items
-      local n_classes = 5
-      local basename = paths.basename(image_path, '.jpg')
-      
-      -- Load image and model
-      local model = load_model(model_path)
-      local img = load_image(image_path)
-      local prepared_img = prepare_image(img)
-      -- image.save("../" .. basename .. "_crop.jpg", prepared_img[1])
+  local command = arg[1]
+  local model_path = arg[2]
+  local model = load_model(model_path)
+  local n_classes = 5
 
-      -- Process class
-      local output = model:forward(prepared_img):squeeze()
-      local classes =  {}
-      local counts =  {}
-      for cut = 1, 10 do 
-        local probs, indexes = output[cut]:topk(n_classes, true, true)
-        for n = 1, n_classes do
-          local class = imagenet[indexes[n]]
-          classes[class] = (classes[class] or 0) + probs[n]
-          counts[class] = (counts[class] or 0) + 1
-        end
+  -- If there are multiple arguments then just process those
+  if command == "image" then
+    for i, image_path in ipairs(arg) do
+      if i > 2 then
+        process(model, image_path, n_classes, i - 2, #arg - 2)
       end
-      
-      -- Print average classes
-      for class, prob in pairs(classes) do
-        classes[class] = prob / counts[class]
-      end
-
-      local features = model.modules[10].output:float()
-      torch.save("../features/" .. basename .. ".t7", {classes, features})
     end
+  end
+
+  -- otherwise process original
+  if command == "original" then 
+    require("original")
+    for i, image_path in ipairs(original) do
+      process(model, image_path, n_classes, i, #original)
+    end
+  end
+
+  -- If we're comparing against older then do so
+  if command == "comapre" then
+    -- require("existing")
   end
 end
 
